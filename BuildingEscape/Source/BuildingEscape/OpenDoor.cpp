18ba08ab54a90673ea09e3e973c63de6190d5357
DESKTOP-KL2LTND\Lancelot <lancelotpp@icloud.com>
2016-12-22 01:19:03 +0100

    V 1.3 Print an ouput warning with string (Name, location) Make rotate an object (door) at game begin

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// POULIN Lancelot Copyright 2016
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor *Owner = GetOwner();
+	FRotator OwnerRotation = Owner->GetActorRotation();
+	FRotator NewRotation = FRotator(0.0f, -70.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
+	UE_LOG(LogTemp, Warning, TEXT("Door Rotation Report: %f"), OwnerRotation.Yaw);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
